a1563542170f92f8d6a11106043fcb6b93e208ca
Jefferson Brandão <jeffersonvideo125@gmail.com>
2021-02-18 14:58:05 -0300

    Working with directory and archive child

---
+++ HomeWork/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+using System.Globalization;
+
+namespace HomeWork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Enter the path of your file: ");
+            string sourcePath = Console.ReadLine();
+
+            try
+            {
+                string[] lines = File.ReadAllLines(sourcePath);
+
+                string targetPath = Path.GetDirectoryName(sourcePath) + @"\out\summary.csv";
+
+                Directory.CreateDirectory(Path.GetDirectoryName(targetPath));
+
+                using (StreamWriter sw = File.AppendText(targetPath))
+                {
+                    foreach (string line in lines)
+                    {
+                        string[] var = line.Split(',');
+
+                        double total = double.Parse(var[1], CultureInfo.InvariantCulture) * int.Parse(var[2]);
+                        sw.WriteLine(var[0] + ", " + total.ToString("F2", CultureInfo.InvariantCulture));
+                    }
+                }
+
+
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("There was an error");
+                Console.WriteLine(e.Message);
+            }
+
+
+
+        }
+    }
+}
